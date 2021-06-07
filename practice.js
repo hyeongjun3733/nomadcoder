96d92496db61b5e3d4cb3bd542ae16df9f15a872
hyeongjun3733 <75615193+hyeongjun3733@users.noreply.github.com>
2021-06-07 22:21:39 +0900

    Add files via upload
    Javascript배우기

---
+++ practice.js
@@ -0,0 +1,34 @@
+let a = 221;
+//const는 변하지 않는 변수 값을 지정할 때, let은 변할 수 있는 변수 값을 지정할 때 
+let b = a - 5;
+let a = 4;
+console.log(b, a)
+
+const dayOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
+// 변수명에 띄워쓰기 대신 대문자로 뒤의 단어 시작해줌
+
+console.log(dayOfWeek)
+
+// object, array 
+const nicoInfo = {
+    name: "Nico",
+    age: 33,
+    gender: 'Male',
+    favMovies: ['Along the gods', 'LOTR', 'Oldboy']
+    favFood: [
+        {
+            name: 'Kimchi',
+            fatty: false
+        }, 
+        {
+            name: Cheese burger, 
+            fatty: true
+        }
+    ]
+}
+
+
+nicoInfo.gender = 'Female'
+// const로 되어 있어도 내용은 수정 가능
+console.log(nicoInfo.gender)
+console.log(nicoInfo.favFood[0].fatty)
